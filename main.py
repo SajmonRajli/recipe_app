d0bb46ae34024f26c77c41f272732fa5ad97de6f
SajmonRajli <obodec1999@mail.ru>
2022-12-12 04:57:35 +0700

    добавил функцию recipe_add и переделал функцию user_get

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 from typing import Union
 from fastapi import FastAPI
 from datetime import datetime, date, time
-
+from pydantic import BaseModel
 from data_base import request_db
 
 app = FastAPI()
@@ -15,13 +15,24 @@ class User:
         self.favorites = favorites
         self.date_of_creation = date_of_creation
         self.date_of_change = date_of_change
-    def to_json(self):
+    def to_json_all(self):
         return {"id": self.id,
                 "nickname": self.nickname,
                 "status": self.status,
                 "favorites": self.favorites,
                 "date_of_creation": self.date_of_creation,
                 "date_of_change": self.date_of_change}
+    def to_json(self, number_of_recipes): #возвращает поля пользователя + кол-во его рецептов
+        return {"id": self.id,
+                "nickname": self.nickname,
+                "status": self.status,
+                "number_of_recipes": number_of_recipes}
+
+
+
+
+
+
 
 @app.get("/")
 def read_root():
@@ -38,7 +49,7 @@ def read_item(item_id: int, q: Union[str, None] = None):
 def user_add(nickname: str):
     text_SQL = f"select nickname from users  where nickname = '{nickname}'"
     res = request_db('recipe_app',text_SQL)
-    if res["response"] != [] and nickname != res["response"][0][0]:
+    if res["response"] == []:
         date_of_creation = date.today()
         id_user = int(datetime.now().timestamp())
         text_SQL = f'''
@@ -51,7 +62,7 @@ def user_add(nickname: str):
     else: 
         return {"response": f'имя мользователя "{nickname}" уже занято'}
 
-# Получениe профиля пользователя
+# Получениe профиля пользователя  
 @app.get("/user/get/{nickname}")
 def user_get(nickname: str):
     text_SQL = f"select * from users  where nickname = '{nickname}'"
@@ -59,6 +70,51 @@ def user_get(nickname: str):
     if res["response"] != []:
         row = res["response"][0]
         user = User(row[0],row[1],row[2],row[3],row[4],row[5])
-        return {"response": user.to_json()}
+        
+        # получаем айди рецептов, что бы посчитать их
+        text_SQL = f"select id from recipes  where author = '{user.id}'"
+        res = request_db('recipe_app',text_SQL)
+        if res["response"] != []:
+            number_of_recipes = len(res["response"])
+        else: number_of_recipes = 0
+
+        return {"response": user.to_json(number_of_recipes)}
     else: 
         return {"response": f'имя мользователя "{nickname}" не найдено'}
+
+
+#Класс для post запроса
+class Recipe(BaseModel): 
+    author: int
+    name: str
+    type_of_dish: str = None
+    description: str = None
+    preparation_steps: str = None
+    photo: str = None
+    hashtags: str = None
+# Добавление пользователем рецепта
+@app.post("/recipe/add/")
+def recipe_add(item: Recipe):
+    date_of_creation = date.today()
+    id_recipe = int(datetime.now().timestamp())
+    likes = 0
+    status = True
+    print(item.author)
+    text_SQL = f'''
+            INSERT INTO recipes (id, author, date_of_creation, name, type_of_dish, description, preparation_steps, photo, likes, hashtags, status)
+            VALUES (
+                {id_recipe}, 
+                {item.author}, 
+                '{date_of_creation}',
+                '{item.name}', 
+                '{item.type_of_dish}', 
+                '{item.description}', 
+                '{item.preparation_steps}', 
+                '{item.photo}', 
+                {likes}, 
+                '{item.hashtags}', 
+                {status})
+            RETURNING *;
+        '''
+    res = request_db('recipe_app',text_SQL)
+    return {"response":res["response"]}
