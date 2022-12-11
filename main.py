fc736d4f3498589519cff38a65b6a1e8189ca01c
SajmonRajli <obodec1999@mail.ru>
2022-12-12 03:26:19 +0700

    добавил функцию для регистрации пользователя user_add

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
 # -*- coding: utf-8 -*-
 from typing import Union
 from fastapi import FastAPI
+from datetime import datetime, date, time
+
+from data_base import request_db
 
 app = FastAPI()
 
-# id int PRIMARY KEY NOT NULL,
-# nickname text NOT NULL,
-# status bool NOT NULL,
-# favorites int[],
-# date_of_creation date NOT NULL,
-# date_of_change date
 
 
 @app.get("/")
@@ -22,3 +19,20 @@ def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
 	
+# Регистрация
+@app.get("/user/add/{nickname}")
+def user_add(nickname: str):
+    text_SQL = f"select nickname from users  where nickname = '{nickname}'"
+    res = request_db('recipe_app',text_SQL)
+    if res["response"] != [] and nickname != res["response"][0][0]:
+        date_of_creation = date.today()
+        id_user = int(datetime.now().timestamp())
+        text_SQL = f'''
+            INSERT INTO users (id, nickname, status, date_of_creation)
+            VALUES ({id_user}, '{nickname}', {True}, '{date_of_creation}')
+            RETURNING *;
+        '''
+        res = request_db('recipe_app',text_SQL)
+        return {"response":res["response"]}
+    else: 
+        return {"response": f'имя мользователя "{nickname}" уже занято'}
