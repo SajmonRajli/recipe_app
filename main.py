a2b517d991748d182a35d107963d9894d12cca90
SajmonRajli <obodec1999@mail.ru>
2022-12-13 01:58:48 +0700

    Добавил функцию для авторизации и ограничил использование функций заблокированными пользователями

--- main.py
+++ main.py
@@ -2,59 +2,10 @@
 from typing import Union
 from fastapi import FastAPI
 from datetime import datetime, date, time
-from pydantic import BaseModel
 from data_base import request_db
-
+from functions import User, Recipe, Recipe_post, get_status_user
 app = FastAPI()
 
-class User:
-    def __init__(self, id, nickname, status, favorites, date_of_creation, date_of_change):
-        self.id = id
-        self.nickname = nickname
-        self.status = status
-        self.favorites = favorites
-        self.date_of_creation = date_of_creation
-        self.date_of_change = date_of_change
-    def to_json_all(self):
-        return {"id": self.id,
-                "nickname": self.nickname,
-                "status": self.status,
-                "favorites": self.favorites,
-                "date_of_creation": self.date_of_creation,
-                "date_of_change": self.date_of_change}
-    def to_json(self, number_of_recipes): #возвращает поля пользователя + кол-во его рецептов
-        return {"id": self.id,
-                "nickname": self.nickname,
-                "status": self.status,
-                "number_of_recipes": number_of_recipes}
-
-class Recipe:
-    def __init__(self, id, author, date_of_creation, date_of_change, name, type_of_dish, description, preparation_steps, photo, likes, hashtags, status):
-        self.id = id
-        self.author = author
-        self.date_of_creation = date_of_creation
-        self.date_of_change = date_of_change
-        self.name = name
-        self.type_of_dish = type_of_dish
-        self.description = description
-        self.preparation_steps = preparation_steps
-        self.photo = photo
-        self.likes = likes
-        self.hashtags = hashtags
-        self.status = status
-    def to_json(self):
-        return {"id": self.id,
-                "author": self.author,
-                "date_of_creation": self.date_of_creation,
-                "date_of_change": self.date_of_change,
-                "name": self.name,
-                "type_of_dish": self.type_of_dish,
-                "description": self.description,
-                "preparation_steps": self.preparation_steps,
-                "photo": self.photo,
-                "likes": self.likes,
-                "hashtags": self.hashtags,
-                "status": self.status}
 
 # Регистрация
 @app.get("/user/add/{nickname}")
@@ -74,36 +25,41 @@ def user_add(nickname: str):
     else: 
         return {"response": f'имя мользователя "{nickname}" уже занято'}
 
-# Получениe профиля пользователя  
-@app.get("/user/get/{nickname}")
-def user_get(nickname: str):
+# Авторизация 
+@app.get("/user/login/{nickname}")
+def user_login(nickname: str):
     text_SQL = f"select * from users  where nickname = '{nickname}'"
     res = request_db('recipe_app',text_SQL)
     if res["response"] != []:
         row = res["response"][0]
         user = User(row[0],row[1],row[2],row[3],row[4],row[5])
-        
-        # получаем айди рецептов, что бы посчитать их
-        text_SQL = f"select id from recipes  where author = '{user.id}'"
-        res = request_db('recipe_app',text_SQL)
-        if res["response"] != []:
-            number_of_recipes = len(res["response"])
-        else: number_of_recipes = 0
+        return {"response": user.to_json_all()}
+    else: 
+        return {"response": f'имя мользователя "{nickname}" не найдено'}
 
-        return {"response": user.to_json(number_of_recipes)}
+# Получениe профиля пользователя  
+@app.get("/user/get/nickname={nickname}&login={login}")
+def user_get(nickname: str, login: int):
+    text_SQL = f"select * from users  where nickname = '{nickname}'"
+    res = request_db('recipe_app',text_SQL)
+    if res["response"] != []:
+        row = res["response"][0]
+        user = User(row[0],row[1],row[2],row[3],row[4],row[5])
+        status = get_status_user(login)
+        if (status == True) or (status == False and login == user.id):
+            # получаем айди рецептов, что бы посчитать их
+            text_SQL = f"select id from recipes  where author = '{user.id}'"
+            res = request_db('recipe_app',text_SQL)
+            if res["response"] != []:
+                number_of_recipes = len(res["response"])
+            else: number_of_recipes = 0
+            return {"response": user.to_json(number_of_recipes)}
+        else:
+            return {"response": 'Вы заблокированы и не можете смотреть других пользователей'}
     else: 
         return {"response": f'имя мользователя "{nickname}" не найдено'}
 
 
-#Класс для post запроса
-class Recipe_post(BaseModel): 
-    author: int
-    name: str
-    type_of_dish: str = None
-    description: str = None
-    preparation_steps: str = None
-    photo: str = None
-    hashtags: str = None
 # Добавление пользователем рецепта
 @app.post("/recipe/add/")
 def recipe_add(item: Recipe_post):
@@ -111,37 +67,43 @@ def recipe_add(item: Recipe_post):
     id_recipe = int(datetime.now().timestamp())
     likes = 0
     status = True
-    print(item.author)
-    text_SQL = f'''
-            INSERT INTO recipes (id, author, date_of_creation, name, type_of_dish, description, preparation_steps, photo, likes, hashtags, status)
-            VALUES (
-                {id_recipe}, 
-                {item.author}, 
-                '{date_of_creation}',
-                '{item.name}', 
-                '{item.type_of_dish}', 
-                '{item.description}', 
-                '{item.preparation_steps}', 
-                '{item.photo}', 
-                {likes}, 
-                '{item.hashtags}', 
-                {status})
-            RETURNING *;
-        '''
-    res = request_db('recipe_app',text_SQL)
-    return {"response":res["response"]}
+    if get_status_user(item.author) == True:
+        text_SQL = f'''
+                INSERT INTO recipes (id, author, date_of_creation, name, type_of_dish, description, preparation_steps, photo, likes, hashtags, status)
+                VALUES (
+                    {id_recipe}, 
+                    {item.author}, 
+                    '{date_of_creation}',
+                    '{item.name}', 
+                    '{item.type_of_dish}', 
+                    '{item.description}', 
+                    '{item.preparation_steps}', 
+                    '{item.photo}', 
+                    {likes}, 
+                    '{item.hashtags}', 
+                    {status})
+                RETURNING *;
+            '''
+        res = request_db('recipe_app',text_SQL)
+        return {"response":res["response"]}
+    else:
+        return {"response": 'Пользователь заблокирован'}
+
 
 # Получениe рецепта
-@app.get("/recipe/get/{id}")
-def recipe_get(id: int):
-    text_SQL = f"select * from recipes  where id = '{id}'"
-    res = request_db('recipe_app',text_SQL)
-    if res["response"] != []:
-        row = res["response"][0]
-        print(row[0])
-        print(row[11])
-        recipe = Recipe(row[0],row[1],row[2],row[3],row[4],row[5],row[6],row[7],row[8],row[9],row[10],row[11])
+@app.get("/recipe/get/{id}&login={login}")
+def recipe_get(id: int, login: int):
+    if get_status_user(login) == True:
+        text_SQL = f"select * from recipes  where id = '{id}'"
+        res = request_db('recipe_app',text_SQL)
+        if res["response"] != []:
+            row = res["response"][0]
+            print(row[0])
+            print(row[11])
+            recipe = Recipe(row[0],row[1],row[2],row[3],row[4],row[5],row[6],row[7],row[8],row[9],row[10],row[11])
 
-        return {"response": recipe.to_json()}
+            return {"response": recipe.to_json()}
+        else: 
+            return {"response": f'рецепт не найден'}
     else: 
-        return {"response": f'рецепт не найден'}
+        return {"response": 'Вы заблокированы и не можете просматривать рецепты'}
